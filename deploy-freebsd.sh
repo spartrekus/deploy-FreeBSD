c5aacabb7e21c98522bd22a29ccd9fea78bbcb5c
spartrekus <spartrekus@gmail.com>
2019-07-14 19:03:06 +0200

    Add files via upload

--- deploy-freebsd.sh
+++ deploy-freebsd.sh
@@ -1,20 +1,41 @@
 
 
+
 echo =========================
 echo "** Expert use only"
 echo "** Fast deployment, without security at all. no warranty "
 echo "** Press Enter"
 echo =========================
+echo disks
+  gpart show | grep da0 
+ls /dev/da0* 
+echo =========================
+mount | grep da0 
+echo =========================
+echo ==Please Enter to Cont ==
+echo =========================
 read abcd
+
+cp deploy-freebsd.sh  /root/deploy-freebsd.sh 
+
+
+
+pkg install -y wget 
 
 echo "MD5 kernel.txz = 0d4ed395541dbeb6ae9001afc351561e"
 echo "MD5 base.txz = 97216020220a816ab689b6ee25bbbd9e"
 echo "MD5 mbr.bin = be7329a1dfff69930ae2c8b12f2a0b6f"
 
+
+
+
 echo Copying mbr with dd...
-wget https://raw.githubusercontent.com/spartrekus/deploy-FreeBSD/master/mbr.bin -O mbr.bin
-md5 mbr.bin
-dd if=mbr.bin of=/dev/da0
+pwd
+
+wget    --no-check-certificate   https://raw.githubusercontent.com/spartrekus/deploy-FreeBSD/master/mbr.bin -O         /tmp/mbr.bin  
+
+md5 /tmp/mbr.bin
+dd if=/tmp/mbr.bin of=/dev/da0
 
 gpart list da0
 
@@ -27,13 +48,27 @@ umount /target
 mkdir /target
 mount /dev/da0s1a  /target
 cd /target
+pwd
+echo =========================
+echo mounted
+mount | grep da0 
+echo =========================
+pwd
 
 
 echo =========================
 echo Fetching the kernel.txz file
-wget "https://netix.dl.sourceforge.net/project/freebsd-12-r328126/r328126-freebsd-dist/usr/freebsd-dist/kernel.txz"    -O kernel.txz 
+pwd
+wget  --no-check-certificate    "https://netix.dl.sourceforge.net/project/freebsd-12-r328126/r328126-freebsd-dist/usr/freebsd-dist/kernel.txz"    -O kernel.txz 
+
 echo Fetching the base.txz file
-wget "https://netcologne.dl.sourceforge.net/project/freebsd-12-r328126/r328126-freebsd-dist/usr/freebsd-dist/base.txz"  -O base.txz 
+pwd
+wget  --no-check-certificate      "https://netcologne.dl.sourceforge.net/project/freebsd-12-r328126/r328126-freebsd-dist/usr/freebsd-dist/base.txz"  -O base.txz 
+
+
+wget --no-check-certificate "https://raw.githubusercontent.com/spartrekus/freebsd-13-entropyfix/master/MANIFEST"     -O MANIFEST   
+
+
 
 
 echo =========================
@@ -46,6 +81,16 @@ echo Uncompress base file
 md5 base.txz 
 tar xpfz base.txz
 
+
+echo =========================
+echo Copy packages base and kernel to install later
+#cp base.txz    /target/usr/freebsd-dist/base.txz  
+#cp kernel.txz  /target/usr/freebsd-dist/kernel.txz 
+#cp MANIFEST  /target/usr/freebsd-dist/MANIFEST  
+echo manifest would be useful to be there
+echo you can run later bsdinstall 
+
+
 echo =========================
 echo Create fstab
 echo "/dev/da0s1a     /               ufs     rw      1       1" > /target/etc/fstab 
@@ -54,27 +99,39 @@ echo "/dev/da0s1b     none            swap    sw      0       0" >> /target/etc/
 
 
 echo =========================
-echo Create rc.conf and wifi wpa config using local etc directory
-cp /etc/rc.conf /target/etc/rc.conf
-cp /etc/wpa_supplicant.conf /target/etc/
+echo Create directories
+mkdir  /target/usr/
+mkdir  /target/usr/freebsd-dist/ 
+mkdir  /target/usr/local/
+mkdir  /target/usr/local/bin/ 
 
-
-echo =========================
-echo Copy wrapper
-
-mkdir /target/usr/local/bin
-cp /usr/local/bin/nconfig  /target/usr/local/bin
-cp /usr/local/bin/nswiss   /target/usr/local/bin
 
 echo =========================
 echo Entropy fix wrapper for lib
-cp /root/libcrypto.so.111  /target/root/
-cp /root/libssl.so.111     /target/root/ 
+echo you will need libcrypto.so.111 into /lib 
+echo you will need libssl.so.111    into /lib 
+echo =========================
+echo Copy wrapper
+cat     /root/bsd/rc.conf >>  /target/etc/rc.conf 
+cp      /root/bsd/wpa*        /target/etc/   
+cp  -r  /root/freebsd-dist/   /target/usr/freebsd-dist/ 
+cp      /root/bsd/nconfig     /target/usr/local/bin
+cp      /root/bsd/nswiss      /target/usr/local/bin
+cp      /root/bsd/libcrypto.so.111    /target/lib/ 
+cp      /root/bsd/libssl.so.111       /target/lib/ 
+echo you will need libcrypto.so.111 into /lib 
+echo you will need libssl.so.111    into /lib 
+echo =========================
+cp -r   /root/bsd/               /target/root/bsd/  
+cp  -r  /root/freebsd-dist/      /target/root/freebsd-dist/ 
+cp      /root/deploy-freebsd.sh  /target/root/deploy-freebsd.sh     
+
+
 
 
 echo =========================
-echo Process Completed
-
+echo Process umount
+echo =========================
 
 echo Umount
 cd /tmp
@@ -82,6 +139,10 @@ umount /target
 umount /target
 cd /tmp
 mount
+
+echo =========================
+echo Process Completed
+echo =========================
 
 echo =========================
 echo "Mission Completed."
